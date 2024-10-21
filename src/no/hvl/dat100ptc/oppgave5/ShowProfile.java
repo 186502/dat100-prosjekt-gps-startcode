209d03cae97824f93d3f6e576e58b560c072b3c9
Åsmund S. <186502@stud.hvl.no>
2024-10-21 22:40:41 +0200

    ShowHeightProfile - v1

--- src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
@@ -7,7 +7,10 @@ import no.hvl.dat100ptc.oppgave1.GPSPoint;
 import no.hvl.dat100ptc.oppgave2.GPSData;
 import no.hvl.dat100ptc.oppgave2.GPSDataConverter;
 import no.hvl.dat100ptc.oppgave2.GPSDataFileReader;
+import no.hvl.dat100ptc.oppgave3.GPSUtils;
 import no.hvl.dat100ptc.oppgave4.GPSComputer;
+import static java.lang.Math.*;
+
 
 import javax.swing.JOptionPane;
 
@@ -45,10 +48,21 @@ public class ShowProfile extends EasyGraphics {
 	public void showHeightProfile(int ybase) {
 		
 		int x = MARGIN; // første høyde skal tegnes ved MARGIN
-		int y;
+		int y = ybase;
+		int N = gpspoints.length;
+		int width = 2 * MARGIN + 3 * N;
+		int step = width/N;
+		
+		for (int i = 0; i<gpspoints.length-1; i++) {
+				int y_base = y;
+				int x_base = x+(i*step);
+				int y_top = y-(int)round(gpspoints[i].getElevation());
+				int x_top = x+(i*step);
+				drawLine(x_base, y_base, x_top, y_top);
+				}
 		
 		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
 		
 
 	}
